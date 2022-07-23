435f406b7d3e19f8591f7230d9aecf17ebc63ca8
yoo hyeong chan <gudcks305@gmail.com>
2022-07-23 23:30:51 +0900

    Update DiscoveryServerApplication.java

--- Discovery-Server/src/main/java/com/dju/gdsc/DiscoveryServerApplication.java
+++ Discovery-Server/src/main/java/com/dju/gdsc/DiscoveryServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-    // Eureka Server에 누구나 접속하지 못하게 security 설정해야함 
+    // Eureka Server에 누구나 접속하지 못하게 security 설정해야함
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
