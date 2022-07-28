fb5aaa15cafc02bba7e8162d5184414a93f5f101
yoo hyeong chan <gudcks305@gmail.com>
2022-07-28 17:25:32 +0900

    feature : gateway docs

---
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.dju.gdsc.domain.common.config;
+
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public CommandLineRunner openApiGroups(
+            RouteDefinitionLocator locator,
+            SwaggerUiConfigParameters swaggerUiParameters) {
+        return args -> locator
+                .getRouteDefinitions().collectList().block()
+                .stream()
+                .map(RouteDefinition::getId)
+                .filter(id -> id.matches(".*-route"))
+                .forEach(swaggerUiParameters::addGroup);
+    }
+}
