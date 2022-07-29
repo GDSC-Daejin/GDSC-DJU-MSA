9b890b3592415b440ba4bbca58633d6c48920d64
yoo hyeong chan <gudcks305@gmail.com>
2022-07-29 16:41:15 +0900

    feature : gateway Swagger Setting
    Gateway에서 모든 Route API 문서를 조회 할 수 있도록 했습니다.

--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/SwaggerConfig.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/SwaggerConfig.java
@@ -1,24 +1,44 @@
 package com.dju.gdsc.domain.common.config;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Configuration
 public class SwaggerConfig {
-
+    // Swagger Gateway 설정 2가지 방법! 
     @Bean
     public CommandLineRunner openApiGroups(
             RouteDefinitionLocator locator,
             SwaggerUiConfigParameters swaggerUiParameters) {
-        return args -> locator
-                .getRouteDefinitions().collectList().block()
+        return args -> Objects.requireNonNull(locator
+                        .getRouteDefinitions().collectList().block())
                 .stream()
                 .map(RouteDefinition::getId)
                 .filter(id -> id.matches(".*-route"))
+                .map(id -> id.replace("-route", ""))
                 .forEach(swaggerUiParameters::addGroup);
     }
+  /*  @Autowired
+    RouteDefinitionLocator locator;
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-route")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-route", "");
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groups;
+    }*/
 }
