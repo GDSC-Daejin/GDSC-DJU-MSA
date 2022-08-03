e7a9a812525570a26c7955733779355de06a4bf9
yoo hyeong chan <gudcks305@gmail.com>
2022-08-03 17:53:33 +0900

    feature : gateway logout

--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/AuthenticationForAdminFilter.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/AuthenticationForAdminFilter.java
@@ -1,8 +1,10 @@
 package com.dju.gdsc.domain.common;
 
+import com.dju.gdsc.domain.auth.service.LogoutService;
 import com.dju.gdsc.global.token.AuthToken;
 import com.dju.gdsc.global.token.AuthTokenProvider;
 import com.dju.gdsc.global.utils.HeaderUtil;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -14,27 +16,31 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 @Component
 @Slf4j
-public class AuthenticationForAdminFilter extends AbstractGatewayFilterFactory<AuthenticationForAllUserFilter.Config> {
+@RequiredArgsConstructor
+public class AuthenticationForAdminFilter extends AbstractGatewayFilterFactory<AuthenticationForAdminFilter.Config> {
     private final AuthTokenProvider authTokenProvider;
-    public AuthenticationForAdminFilter(AuthTokenProvider authTokenProvider) {
-        super(AuthenticationForAllUserFilter.Config.class);
-        this.authTokenProvider = authTokenProvider;
-    }
+    private final LogoutService logoutService;
 
     @Override
-    public GatewayFilter apply(AuthenticationForAllUserFilter.Config config) {
+    public GatewayFilter apply(AuthenticationForAdminFilter.Config config) {
         return (exchange, chain) -> {
+
             ServerHttpRequest request = exchange.getRequest();
             if(!request.getHeaders().containsKey("Authorization")){
                 return handleUnAuthorized(exchange);
             }
             String accessToken = HeaderUtil.getAccessToken(request);
             AuthToken authToken = authTokenProvider.convertAuthToken(accessToken);;
-
             if (!authToken.validate()) {
                 exchange.getResponse().setStatusCode(HttpStatus.valueOf(401));
                 return exchange.getResponse().setComplete();
             }
+            if(logoutService.logout(accessToken) != null){
+                return handleUnAuthorized(exchange);
+            }
+            logoutService.logIn(accessToken);
+
+
             if(!authToken.getTokenClaims().get("role").equals("LEAD") | !authToken.getTokenClaims().get("role").equals("CORE")) {
                 exchange.getResponse().setStatusCode(HttpStatus.valueOf(403));
                 log.info("User is not admin , REQUEST ROLE " + authToken.getTokenClaims().get("role")
--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/AuthenticationForAllUserFilter.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/AuthenticationForAllUserFilter.java
@@ -1,5 +1,6 @@
 package com.dju.gdsc.domain.common;
 
+import com.dju.gdsc.domain.auth.service.LogoutService;
 import com.dju.gdsc.global.token.AuthToken;
 import com.dju.gdsc.global.token.AuthTokenProvider;
 import com.dju.gdsc.global.utils.HeaderUtil;
@@ -18,13 +19,13 @@ import reactor.core.publisher.Mono;
 import java.util.Objects;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class AuthenticationForAllUserFilter extends AbstractGatewayFilterFactory<AuthenticationForAllUserFilter.Config> {
     private final AuthTokenProvider authTokenProvider;
-    public AuthenticationForAllUserFilter(AuthTokenProvider authTokenProvider) {
-        super(Config.class);
-        this.authTokenProvider = authTokenProvider;
-    }
+    private final LogoutService logoutService;
+
+    
 
     @Override
     public GatewayFilter apply(Config config) {
@@ -39,7 +40,10 @@ public class AuthenticationForAllUserFilter extends AbstractGatewayFilterFactory
                 exchange.getResponse().setStatusCode(HttpStatus.valueOf(401));
                 return exchange.getResponse().setComplete();
             }
-
+            if(logoutService.logout(accessToken) != null){
+                return handleUnAuthorized(exchange);
+            }
+            logoutService.logIn(accessToken);
             ServerHttpRequest newRequest = request.mutate()
                     .header("userId", (String) authToken.getTokenClaims().get("sub"))
                     .build();
