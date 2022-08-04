537d5321e807f8d32e2b9dc916c311d1ab47ea76
yoo hyeong chan <gudcks305@gmail.com>
2022-08-05 01:35:49 +0900

    Update CorsConfig.java

--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
@@ -27,7 +27,7 @@ public class CorsConfig {
             if (CorsUtils.isCorsRequest(request)) {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
-                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+              //  headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
                 headers.add("Access-Control-Max-Age", MAX_AGE); //OPTION how long the results of a preflight request (that is the information contained in the Access-Control-Allow-Methods and Access-Control-Allow-Headers headers) can be cached.
                 headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
