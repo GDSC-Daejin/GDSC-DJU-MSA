3096986b867f66c6c8a5db66063b161ea6a3a01e
yoo hyeong chan <gudcks305@gmail.com>
2022-08-05 12:14:10 +0900

    Update CorsConfig.java

--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
@@ -15,8 +15,12 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class CorsConfig {
-    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
+
+    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN";
+    private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS";
     private static final String ALLOWED_ORIGIN = "*";
+    private static final String MAX_AGE = "3600";
+
     @Bean
     public WebFilter corsFilter() {
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
@@ -26,7 +30,8 @@ public class CorsConfig {
                 HttpHeaders headers = response.getHeaders();
                 headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Allow-Headers", "*");
+                headers.add("Access-Control-Max-Age", MAX_AGE);
+                headers.add("Access-Control-Allow-Headers",ALLOWED_HEADERS);
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);
                     return Mono.empty();
@@ -35,4 +40,5 @@ public class CorsConfig {
             return chain.filter(ctx);
         };
     }
+
 }
