a507328d5d1503131843ab428827c273b0cc12b7
yoo hyeong chan <gudcks305@gmail.com>
2022-08-05 12:06:19 +0900

    Update CorsConfig.java

--- API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
+++ API-Gateway/src/main/java/com/dju/gdsc/domain/common/config/CorsConfig.java
@@ -15,11 +15,8 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 public class CorsConfig {
-    private static final String ALLOWED_HEADERS = "authorization, Content-Type, Content-Length, Authorization, credential, X-XSRF-TOKEN";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
     private static final String ALLOWED_ORIGIN = "*";
-    private static final String EXPOSE_HEADERS = "*, Authorization";
-    private static final String MAX_AGE = "7200"; //2 hours (2 * 60 * 60)
     @Bean
     public WebFilter corsFilter() {
         return (ServerWebExchange ctx, WebFilterChain chain) -> {
@@ -27,10 +24,9 @@ public class CorsConfig {
             if (CorsUtils.isCorsRequest(request)) {
                 ServerHttpResponse response = ctx.getResponse();
                 HttpHeaders headers = response.getHeaders();
-              //  headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+                headers.add("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
                 headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
-                headers.add("Access-Control-Max-Age", MAX_AGE); //OPTION how long the results of a preflight request (that is the information contained in the Access-Control-Allow-Methods and Access-Control-Allow-Headers headers) can be cached.
-                headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+                headers.add("Access-Control-Allow-Headers", "*");
                 if (request.getMethod() == HttpMethod.OPTIONS) {
                     response.setStatusCode(HttpStatus.OK);
                     return Mono.empty();
