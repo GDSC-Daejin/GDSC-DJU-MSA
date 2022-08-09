e7b9ec8212f6ad563e0158fb5821238317775f42
yoo hyeong chan <gudcks305@gmail.com>
2022-08-09 15:08:38 +0900

    Update SlackMemberApiController.java

--- Auth-Server/src/main/java/com/dju/gdsc/domain/member/controller/SlackMemberApiController.java
+++ Auth-Server/src/main/java/com/dju/gdsc/domain/member/controller/SlackMemberApiController.java
@@ -27,7 +27,7 @@ public class SlackMemberApiController {
         return Response.success("data" , slackMemberService.getSlackMember());
     }
     @Operation(summary = "슬랙 사용자 정보 전체 동기화" , description = "모든 사용자 정보를 슬랙 정보와 매칭 시키는 작업 , 닉네임으로 매칭 시킴")
-    @PostMapping("/member-route/api/admin/v1/slackMember")
+    @PostMapping("/api/admin/v1/slackMember")
     public Response postSlackMember() throws  SlackApiException, IOException {
         slackMemberService.synchronizationSlackMemberWithServerFirst();
         return Response.success("message" , HttpStatus.OK);
