67bf786e7b02b2fecc23b882c5ee7eb13daec5fd
yoo hyeong chan <84506708+gudcks0305@users.noreply.github.com>
2022-09-12 03:25:11 +0900

    feature : cors config for cookies

--- Auth-Server/src/main/java/com/dju/gdsc/domain/common/config/corsConfig/CorsConfigNotReal.java
+++ Auth-Server/src/main/java/com/dju/gdsc/domain/common/config/corsConfig/CorsConfigNotReal.java
@@ -16,9 +16,11 @@ public class CorsConfigNotReal implements CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); //  내 서버가 응답할때 json을 자바스크립트가 처리할수있게 할건지
         config.addAllowedOriginPattern("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*"); // 모든 헤더 응답허용
         config.addAllowedMethod("*"); // 모든 post get put delete 요청 허용
         config.addExposedHeader("Set-Cookie");
+        config.addExposedHeader("Access-Control-Allow-Origin");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
